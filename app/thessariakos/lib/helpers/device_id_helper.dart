edcb95a27e8d98640a5d87e654866bcd29d216fa
orestislef <orestis_94@hotmail.com>
2024-01-16 17:07:05 +0200

    added getDeviceId for web (NOT GOOD biaih)

--- app/thessariakos/lib/helpers/device_id_helper.dart
+++ app/thessariakos/lib/helpers/device_id_helper.dart
@@ -1,4 +1,6 @@
+import 'dart:html' as html;
 import 'dart:io' show Platform;
+import 'dart:math';
 
 import 'package:device_info/device_info.dart';
 import 'package:flutter/foundation.dart';
@@ -10,7 +12,9 @@ class DeviceIdHelper {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
     try {
-      if (Platform.isAndroid) {
+      if (kIsWeb) {
+        deviceId = await _getWebDeviceId();
+      } else if (Platform.isAndroid) {
         deviceId = await _getAndroidDeviceId(deviceInfo);
       } else if (Platform.isIOS) {
         deviceId = await _getIosDeviceId(deviceInfo);
@@ -24,7 +28,6 @@ class DeviceIdHelper {
     return deviceId;
   }
 
-
   static Future<String> _getAndroidDeviceId(DeviceInfoPlugin deviceInfo) async {
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
     return androidInfo.androidId;
@@ -33,5 +36,23 @@ class DeviceIdHelper {
   static Future<String> _getIosDeviceId(DeviceInfoPlugin deviceInfo) async {
     IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
     return iosInfo.identifierForVendor;
+  }
+
+  static Future<String> _getWebDeviceId() async {
+    const key = 'uniqueId';
+    var storedId = html.window.localStorage[key];
+
+    if (storedId == null) {
+      storedId = _generateRandomId();
+      html.window.localStorage[key] = storedId;
+    }
+
+    return storedId;
+  }
+
+  static String _generateRandomId() {
+    final random = Random();
+    final id = '${random.nextInt(999999)}-${random.nextInt(999999)}';
+    return id;
   }
 }
