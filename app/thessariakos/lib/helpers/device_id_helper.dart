ffbd2a98233367a4b1d1b03ce9e60ca796489475
orestislef <orestis_94@hotmail.com>
2024-01-25 15:36:26 +0200

    added device id for web

--- app/thessariakos/lib/helpers/device_id_helper.dart
+++ app/thessariakos/lib/helpers/device_id_helper.dart
@@ -2,32 +2,27 @@ import 'dart:io' show Platform;
 
 import 'package:device_info/device_info.dart';
 import 'package:flutter/foundation.dart';
+import 'package:platform_device_id/platform_device_id.dart';
 
 class DeviceIdHelper {
   static Future<String> getDeviceId() async {
-    String deviceId = '';
-
     if (kIsWeb) {
-      return 'web';
+      String? deviceId = await PlatformDeviceId.getDeviceId;
+      return deviceId ?? 'web';
     }
 
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
-    try {
-      if (Platform.isAndroid) {
-        deviceId = await _getAndroidDeviceId(deviceInfo);
-      } else if (Platform.isIOS) {
-        deviceId = await _getIosDeviceId(deviceInfo);
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error getting device ID: $e');
-      }
+    if (Platform.isAndroid) {
+      String deviceId = await _getAndroidDeviceId(deviceInfo);
+      return deviceId;
+    } else if (Platform.isIOS) {
+      String deviceId = await _getIosDeviceId(deviceInfo);
+      return deviceId;
     }
 
-    return deviceId;
+    return 'unknown';
   }
-
 
   static Future<String> _getAndroidDeviceId(DeviceInfoPlugin deviceInfo) async {
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
