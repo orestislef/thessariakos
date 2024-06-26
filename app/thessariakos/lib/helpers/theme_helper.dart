66f39ba1bd47d5a59861bdf60cf9dcfd5ea965f6
orestislef <47128201+orestislef@users.noreply.github.com>
2024-06-26 13:01:36 +0300

    Delete app directory

--- app/thessariakos/lib/helpers/theme_helper.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ThemeHelper {
-  static bool isDarkMode(BuildContext context) {
-    return MediaQuery.of(context).platformBrightness == Brightness.dark;
-  }
-}
